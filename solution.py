379bb23140fa682d2c9f462eeedfbfbd73fc12ff
Vladimir Grebnev <36560479+Grelodya@users.noreply.github.com>
2023-04-12 23:35:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from hyppo.ksample import MMD
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 382319199 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    if MMD(compute_kernel="poly").test(x, y)[1] < 0.01:
+        answer = True
+    else:
+        answer = False
+    return answer
